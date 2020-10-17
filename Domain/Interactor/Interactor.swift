53a1905eb7decab1040a2aa47375415c0b0c1059
Jhonatahan Rivera <jonathan02rv@gmail.com>
2020-10-17 11:13:30 -0500

    interactor implements

---
+++ Domain/Interactor/Interactor.swift
@@ -0,0 +1,16 @@
+//
+//  Interactor.swift
+//  JRListArticle
+//
+//  Created by Jhonatahan on 10/17/20.
+//
+
+import Foundation
+
+public class Interactor{
+    var repository : Any
+    
+    public init(repository:Any){
+        self.repository = repository
+    }
+}
---
+++ Domain/Interactor/Services/ArticlesInteractor.swift
@@ -0,0 +1,27 @@
+//
+//  ArticlesInteractor.swift
+//  JRListArticle
+//
+//  Created by Jhonatahan on 10/17/20.
+//
+
+import Foundation
+
+public protocol ArticlesInteractorProtocol{
+    func getListArticles(completion: @escaping (Result<ArticleModel, Error>) -> Void)
+}
+
+public class ArticlesInteractor:Interactor, ArticlesInteractorProtocol{
+    public func getListArticles(completion: @escaping (Result<ArticleModel, Error>) -> Void) {
+        return(self.repository as! ArticlesInteractorProtocol).getListArticles { (result) in
+            switch result{
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    
+}
